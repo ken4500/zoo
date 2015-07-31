5b06423c8dae9841d44b46f69e4e047b18faf1b4
ken.watanabe <ken.watanabe@gree.co.jp>
2015-07-31 00:16:39 -0700

    remove CCLOG in CSLoader

--- cocos2d/cocos/editor-support/cocostudio/ActionTimeline/CSLoader.cpp
+++ cocos2d/cocos/editor-support/cocostudio/ActionTimeline/CSLoader.cpp
@@ -255,7 +255,6 @@ Node* CSLoader::createNode(const std::string& filename)
     std::string path = filename;
     size_t pos = path.find_last_of('.');
     std::string suffix = path.substr(pos + 1, path.length());
-    CCLOG("suffix = %s", suffix.c_str());
     
     CSLoader* load = CSLoader::getInstance();
     
@@ -860,7 +859,6 @@ Node* CSLoader::nodeWithFlatBuffersFile(const std::string &fileName, const ccNod
     // decode plist
     auto textures = csparsebinary->textures();
     int textureSize = csparsebinary->textures()->size();
-    CCLOG("textureSize = %d", textureSize);
     for (int i = 0; i < textureSize; ++i)
     {
         SpriteFrameCache::getInstance()->addSpriteFramesWithFile(textures->Get(i)->c_str());
@@ -882,7 +880,6 @@ Node* CSLoader::nodeWithFlatBuffers(const flatbuffers::NodeTree *nodetree, const
         Node* node = nullptr;
         
         std::string classname = nodetree->classname()->c_str();
-        CCLOG("classname = %s", classname.c_str());
         
         auto options = nodetree->options();
         
@@ -891,7 +888,6 @@ Node* CSLoader::nodeWithFlatBuffers(const flatbuffers::NodeTree *nodetree, const
             auto reader = ProjectNodeReader::getInstance();
             auto projectNodeOptions = (ProjectNodeOptions*)options->data();
             std::string filePath = projectNodeOptions->fileName()->c_str();
-            CCLOG("filePath = %s", filePath.c_str());
             
             cocostudio::timeline::ActionTimeline* action = nullptr;
             if (filePath != "" && FileUtils::getInstance()->isFileExist(filePath))
@@ -967,12 +963,10 @@ Node* CSLoader::nodeWithFlatBuffers(const flatbuffers::NodeTree *nodetree, const
         
         auto children = nodetree->children();
         int size = children->size();
-        CCLOG("size = %d", size);
         for (int i = 0; i < size; ++i)
         {
             auto subNodeTree = children->Get(i);
             Node* child = nodeWithFlatBuffers(subNodeTree, callback);
-            CCLOG("child = %p", child);
             if (child)
             {
                 PageView* pageView = dynamic_cast<PageView*>(node);
